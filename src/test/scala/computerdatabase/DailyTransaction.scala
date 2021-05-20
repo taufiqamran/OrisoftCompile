5ec4fc61f6bd9cc9b8f5600c3ede58851dbb1933
taufiq3 <taufiq@seesharpsolutions.com>
2021-05-20 18:00:59 +0800

    Add Report Refactor & Attach File

--- src/test/scala/computerdatabase/DailyTransaction.scala
+++ src/test/scala/computerdatabase/DailyTransaction.scala
@@ -7,6 +7,8 @@ import scala.concurrent.duration._
 import computerdatabase.Module.Login._
 import computerdatabase.Module.Leave._
 import computerdatabase.Module.Claim._
+import computerdatabase.Module.Profile.Report._
+import computerdatabase.Module.Leave.Report._
 
 
 class DailyTransaction extends Simulation{
@@ -20,11 +22,15 @@ class DailyTransaction extends Simulation{
 
   val loginSupFeeder1 = csv("data/superior_Credential.csv").queue
 
+  val loginHrFeeder = csv("data/hr_Credential.csv").queue
+
   //Login Employee
   val login = feed(loginFeeder1).exec(Login.loginProcess).exec(Login.loadDashboard)
 
   //Login Superior
   val loginSup = feed(loginSupFeeder1).exec(Login.loginProcess).exec(Login.loadDashboard)
+
+  val loginHr = feed(loginHrFeeder).exec(Login.loginProcess).exec(Login.loadDashboard)
 
   //EMP
   val ALSaveDraftex = exec(ESSEMPApplyLeave.loadLeaveForm,ESSEMPApplyLeave.saveLeaveDraft,ESSEMPApplyLeave.leaveLogout)
@@ -35,10 +41,27 @@ class DailyTransaction extends Simulation{
 
   //SUP
   val MyTeamViewClaimEntitlementex = exec(ESSSUPMyTeamViewClaimEntitlement.LoadClaimEntitlement,ESSSUPMyTeamViewClaimEntitlement.searchClaimEntitlement   ,ESSSUPMyTeamViewClaimEntitlement.logout)
+  val MyTeamViewLeaveEntitlementex = exec(ESSSUPMyTeamViewLeaveEntitlement.loadLeaveEntitlement,ESSSUPMyTeamViewLeaveEntitlement.clickLeaveEntitlement   ,ESSSUPMyTeamViewLeaveEntitlement.logout)
+  val MyTeamViewLeaveApplicationex = exec(ESSSUPMyTeamViewLeaveApplication.loadLeaveApplication,ESSSUPMyTeamViewLeaveApplication.searchLeave,ESSSUPMyTeamViewLeaveApplication.clickRequestId,ESSSUPMyTeamViewLeaveApplication.logout)
+  val MyTeamViewClaimApplicationex = exec(ESSSUPMyTeamViewClaimApplication.loadViewClaimApplication,ESSSUPMyTeamViewClaimApplication.clickRequestId,ESSSUPMyTeamViewClaimApplication.closeClaimApplication,ESSSUPMyTeamViewClaimApplication.logout)
+
+  //HR
+  val EmployeeListingReportex = exec(EmployeeListingReport.loadReport,EmployeeListingReport.getReport,EmployeeListingReport.logout)
+  val EmployeeMovementReportex = exec(EmployeeMovementReport.loadMovementReport,EmployeeMovementReport.FilterForm,EmployeeMovementReport.clickSearch,EmployeeMovementReport.logout)
+  val FixedTransactionListingex = exec(FixedTransactionListing.loadFixedTransaction,FixedTransactionListing.selectFilter,FixedTransactionListing.clickSearch,FixedTransactionListing.logout)
+  val EmployeeLeaveApproverListingex = exec(EmployeeLeaveApproverListingReport.loadLeaveApproverListing,EmployeeLeaveApproverListingReport.searchFilter,EmployeeLeaveApproverListingReport.clickSearch,EmployeeLeaveApproverListingReport.logout)
 
 
   val debug = exec(session => {
 
+    //val leaveDetail = session("ViewBalance").as[String]
+    //println(s"Leave Detail : $leaveDetail")
+
+    val report = session("ReportViewer").as[String]
+    println(s"Report : $report")
+
+    val huhu = session("BodyCriteria").as[String]
+    //println(s"Body : $huhu")
     //val bodyClaimListing = session("BodyClaim").as[String]
     //println(s"Body Claim Listing : $bodyClaimListing")
 
@@ -62,7 +85,18 @@ class DailyTransaction extends Simulation{
 
   //SUP
   val MyTeamViewClaimEntitlement = scenario("My Team View Claim Entitlement").exec(loginSup,MyTeamViewClaimEntitlementex,debug)
+  val MyTeamViewLeaveEntitlement = scenario("My Team View Leave Entitlement").exec(loginSup,MyTeamViewLeaveEntitlementex,debug)
+  val MyTeamViewLeaveApplication = scenario("My Team View Leave Application").exec(loginSup,MyTeamViewLeaveApplicationex,debug)
+  val MyTeamViewClaimApplication = scenario("My Team View Claim Application").exec(loginSup,MyTeamViewClaimApplicationex,debug)
 
-  setUp(MyTeamViewClaimEntitlement.inject(atOnceUsers(1))).protocols(httpProtocol)
+  //HR Profile
+  val ProfileEmployeeListingReportS = scenario("Profile Employee Listing Report").exec(loginHr,EmployeeListingReportex,debug)
+  val ProfileEmployeeMovementReportS = scenario("Profile Employee Movement Report").exec(loginHr,EmployeeMovementReportex,debug)
+  val ProfileFixedTransactionListingS = scenario("Profile Fixed Transaction Listing Report").exec(loginHr,FixedTransactionListingex,debug)
+  val LeaveEmployeeApproverListingS = scenario("Leave Employee Approver Listing Report").exec(loginHr,EmployeeLeaveApproverListingex,debug)
+
+
+
+  setUp(LeaveEmployeeApproverListingS.inject(atOnceUsers(1))).protocols(httpProtocol)
 
 }
